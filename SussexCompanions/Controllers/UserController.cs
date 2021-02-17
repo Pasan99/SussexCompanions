e338146d8472b19896873379ed8368b64c253912
pasan_manohara <pasanmanohara2000@gmail.com>
2021-02-17 13:15:50 +0530

    Create Event

---
+++ SussexCompanions/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SussexCompanions.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ SussexCompanions/Models/ViewModels/Common/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SussexCompanions.Models.ViewModels.Common
+{
+    public class EditUserViewModel
+    {
+        public User User { get; set; }
+        public UserDetail UserDetail { get; set; }
+    }
+}
