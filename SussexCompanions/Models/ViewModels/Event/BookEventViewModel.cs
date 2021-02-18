6242aa112be91e9dde784ba54497e7458ae78170
pasan_manohara <pasanmanohara2000@gmail.com>
2021-02-18 14:05:00 +0530

    Book Events

---
+++ SussexCompanions/Models/ViewModels/Event/BookEventViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SussexCompanions.Models.ViewModels.Event
+{
+    public class BookEventViewModel
+    {
+        public UserEvent UserEvent { get; set; }
+        public User User { get; set; }
+        public Models.Event Event { get; set; }
+
+    }
+}
